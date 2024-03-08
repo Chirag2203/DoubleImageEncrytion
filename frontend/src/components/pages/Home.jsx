3dd0ce1909da9c6bd0855fdf9ab8ec4870d6ea0f
Chirag Rajput <chirag.3r@gmail.com>
2024-03-08 16:22:33 +0530

    flask setup

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from '../shared/Hero'
+import axios from 'axios'
 
 const Home = () => {
+const [data, setData] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:5000/',
+      );
+      console.log(result);
+      setData(result.data);
+    };
+    fetchData();
+
+    
+  }, [])
+
+
   return (
     <div className='bg-hero-pattern bg-cover bg-no-repeat  min-h-screen'>
         <Hero/>
+        <div className='text-white'>
+        </div>
       
     </div>
   )
